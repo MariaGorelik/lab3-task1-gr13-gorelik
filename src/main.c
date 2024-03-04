674abc886ea985351b0d91b10f433d0aae310b43
MARYIA HARELIK <147317867+MariaGorelik@users.noreply.github.com>
2024-03-04 18:04:34 +0000

    Change main.c according to the task

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "prime_twins.h"
 
-void main (void)
-{
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+int main(int argc, char *argv[]) {
+    int limit;
+
+    if (argc != 2) {
+        printf("Usage: %s <limit>\n", argv[0]);
+        return 1; // Returning a non-zero error code
+    }
+
+    limit = atoi(argv[1]); // Converting a string to an integer
+
+    if (limit <= 0) {
+        printf("Limit must be a positive integer.\n");
+        return 1; // Returning a non-zero error code
+    }
+
+    printTwinPrimes(limit);
+
+    return 0;
 }
